fc081d41ded803622227073a13fe92ccd0320ca8
Ulrich Schwickerath <ulrich.schwickerath@gmail.com>
2023-07-02 08:55:25 +0200

    cosmetical changes

--- include/config.h
+++
@@ -1,5 +0,0 @@
-/* include/config.h.  Generated from config.h.in by configure.  */
-/* 
-   config.h
-*/
-
--- include/parseterm.hpp
+++ include/parseterm.hpp
@@ -1,12 +1,23 @@
 /*
- * $Id: parsetemr.hpp,v 1.5 2004/01/20 19:54:33 ulrich Exp $
+ * $Id: parseterm.cpp,v 1.5 2004/01/20 19:54:33 ulrich Exp $
  * Copyright (C) Ulrich Schwickerath
- *  This file is part of lcal, a command line calculator 
- *  Author: Ulrich Schwickerath, (C) 12/2000 CERN
- *          ulrich.schwickerath@web.de
- *  
- *  This code comes with absolutly no warranty!
- *  it should not be used for production purposes whatsoever
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * This code comes with absolutly no warranty!
+ * it should not be used for production purposes whatsoever
  *
  */
 
